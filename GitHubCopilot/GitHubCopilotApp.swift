9be3746133d1bc23b790bd5c3600a4809c5df6e0
Philip Keller <philipjoelkeller@gmail.com>
2023-03-07 14:31:57 -0800

    Initial Commit

---
+++ GitHubCopilot/GitHubCopilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCopilotApp.swift
+//  GitHubCopilot
+//
+//  Created by Philip Keller on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCopilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
